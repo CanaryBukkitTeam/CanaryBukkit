204dbbefaa787f6806e6fa1a25d85c776a399192
Jamie (Lexteam) <lexwaresupport@outlook.com>
2014-09-11 21:16:56 +0100

    It usually works, if you stage the changed files.

---
+++ canarybukkit/src/main/java/lexteam/minecraft/canarybukkit/CanaryBukkit.java
@@ -0,0 +1,17 @@
+package lexteam.minecraft.canarybukkit;
+
+import net.canarymod.plugin.Plugin;
+
+public class CanaryBukkit extends Plugin {
+
+	@Override
+	public boolean enable() {
+		return false;
+	}
+
+	@Override
+	public void disable() {
+		
+	}
+
+}
