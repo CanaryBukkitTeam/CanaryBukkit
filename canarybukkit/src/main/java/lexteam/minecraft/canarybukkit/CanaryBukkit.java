b530c0cc5e48c609bd2022cf3fc784b48449af88
Jamie (Lexteam) <lexwaresupport@outlook.com>
2014-09-12 22:16:31 +0100

    Hmm

--- canarybukkit/src/main/java/lexteam/minecraft/canarybukkit/CanaryBukkit.java
+++ canarybukkit/src/main/java/lexteam/minecraft/canarybukkit/CanaryBukkit.java
@@ -1,11 +1,14 @@
 package lexteam.minecraft.canarybukkit;
 
+import org.bukkit.Bukkit;
+
 import net.canarymod.plugin.Plugin;
 
 public class CanaryBukkit extends Plugin {
 
 	@Override
 	public boolean enable() {
+		getLogman().info("This server is running " + Bukkit.getName() + " version " + Bukkit.getVersion() + " (Implementing API version " + Bukkit.getBukkitVersion() + ")");
 		return false;
 	}
 
